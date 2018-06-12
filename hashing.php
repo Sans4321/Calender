b40299e205ca658b9abe035d3da6838c5eddcf72
Aakanksha2810 <39698493+Aakanksha2810@users.noreply.github.com>
2018-06-12 23:13:27 +0530

    Add files via upload

---
+++ hashing.php
@@ -0,0 +1,19 @@
+
+<html>
+<body>
+<?php
+function create_hash()
+{
+    $pwd = $password;
+    $salt = " -+457fhdkd950dd#%%^*&"  ;
+    $hash_md5 = md5($salt.$pwd);
+    echo "original password :" .$pwd. "<br>";
+    echo "original salt :" .$salt. "<br>";
+    echo "MD5 with salt :" .$hash_md5."<br>";
+    
+}
+create_hash();    
+ 
+?>
+</body>
+</html> 
