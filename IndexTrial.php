bbb3a3e6079b47a119da2d043238d3a5f53dec0c
Aakanksha2810 <39698493+Aakanksha2810@users.noreply.github.com>
2018-07-03 10:16:42 +0530

    Add files via upload

---
+++ IndexTrial.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>insert color</title>
+</head>
+<body>
+<form action="insert.php" method="post";>
+    <tr>
+        <td>color:</td>
+        <td>
+    <input type="radio" name="color" value="blue">blue
+     <input type="radio" name="color" value="black">black
+     <input type="radio" name="color" value="red">red
+        </td>
+    <tr>
+        <td><input type="submit" value="done"></td>
+    </tr>
+    </form>
+    </body>
+</html>
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
+                                      
